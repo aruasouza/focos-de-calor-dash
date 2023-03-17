412994e99453b7f62d77fdbac7a7980a2145fc37
aruasouza <arua.viso@gmail.com>
2023-03-17 11:20:21 -0300

    changes

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ import urllib
 app = Dash(__name__)
 
 font_color = 'rgb(100,100,100)'
-map_style = "stamen-terrain"
 
 def get_data():
     url = 'http://queimadas.dgi.inpe.br/api/focos'
@@ -62,24 +61,25 @@ def inicial_figure():
     fig = px.density_mapbox(pd.DataFrame({'Município':[],'Latitude':[],'Longitude':[],'País':[],'Estado':[]}),
                             lat="Latitude", lon="Longitude",radius = 4, hover_name="Município",
                             hover_data=["País",'Estado'], zoom=3,color_continuous_scale = 'Hot')
-    fig.update_layout(mapbox_style=map_style,transition_duration=500,margin=dict(l=0,r=0,b=0,t=0))
+    fig.update_layout(mapbox_style='stamen-terrain',transition_duration=500,margin=dict(l=0,r=0,b=0,t=0))
     return fig
 
-@app.callback(Output('mapa','figure'),Input('data','data'))
-def update_figure(data):
-    df = pd.read_json(data, orient='split')
-    fig = px.density_mapbox(df, lat="Latitude", lon="Longitude",radius = 4, hover_name="Município",
-        hover_data=["País",'Estado','Data','Hora'], zoom=3,color_continuous_scale = 'Hot')
-
-    fig.update_layout(mapbox_style=map_style,transition_duration=500,margin=dict(l=0,r=0,b=0,t=0))
-    fig.update(layout_coloraxis_showscale=False)
-    return fig
-
-@app.callback(Output('data', 'data'), Input('dummy', 'children'), State('data', 'data'))
-def update_data(dummy, data):
+@app.callback(Output('data', 'data'),Output('mapa','figure'), Input('modo_escuro', 'value'), State('data', 'data'),State('mapa','figure'))
+def update_data(value, data, fig):
     if not data or not callback_context.triggered:
         data = get_data()
-    return data.to_json(orient='split')
+        fig = px.density_mapbox(data, lat="Latitude", lon="Longitude",radius = 4, hover_name="Município",
+        hover_data=["País",'Estado','Data','Hora'], zoom=3,color_continuous_scale = 'Hot')
+        fig.update_layout(mapbox_style = 'stamen-terrain',transition_duration=500,margin=dict(l=0,r=0,b=0,t=0))
+        fig.update(layout_coloraxis_showscale=False)
+        data = data.to_json(orient='split')
+        return data,fig
+    fig = go.Figure(fig)
+    if value == ['Modo Escuro']:
+        fig.update_layout(mapbox_style="carto-darkmatter")
+    else:
+        fig.update_layout(mapbox_style = 'stamen-terrain')
+    return data,fig
 
 @app.callback(Output('baixar_mapa','href'),Input('mapa','figure'))
 def create_html(figure):
@@ -102,33 +102,60 @@ def show_info(open,close):
         return {'display':'block'},{'display':'block'},True
     return {'display': 'none'},{'display': 'none'},False
 
+@app.callback(Output('dashboard','style'),Output('fade2','style'),Output('dashButton','disabled'),Input('dashButton','n_clicks'),Input('closeDash','n_clicks'))
+def show_dash(open,close):
+    if open > close:
+        return {'display':'block'},{'display':'block'},True
+    return {'display': 'none'},{'display': 'none'},False
+
+@app.callback(Output('byCountry','figure'),Output('bySatelite','figure'),Input('data','data'))
+def build_graphs(data):
+    df = pd.read_json(data, orient='split')
+    df['Focos'] = 1
+    group1 = df[['País','Focos']].groupby('País').count().sort_values('Focos',ascending=False)
+    print(group1)
+    fig1 = go.Figure(go.Bar(x = group1.index,y = group1['Focos']))
+    group2 = df[['Satélite','Focos']].groupby('Satélite').count().sort_values('Focos',ascending=False)
+    fig2 = go.Figure(go.Bar(x = group2.index,y = group2['Focos']))
+    return fig1,fig2
+
 app.layout = html.Div([
-    html.Div(id='dummy', style={'display': 'none'}),
     dcc.Store(id = 'data'),
     html.H1('Focos de Calor na América do Sul',
             style = {'font-family':'helvetica','background-color':'white','border-radius':'5px',
                         'padding':'10px','color':font_color,'margin':'10px','float':'left','font-size':'23px'},id = 'title'),
     html.Div([
-        html.A(html.Button('Sobre',id = 'sobre',className = 'menuButton',n_clicks = 0)),
-        html.A(html.Button('Baixar Dados',className = 'menuButton'),id = 'baixar_dados',download = 'focos_de_calor.csv'),
-        html.A(html.Button('Baixar Mapa',className = 'menuButton'),id = 'baixar_mapa',download = 'focos_de_calor.html')],
-        style = {'position': 'fixed', 'top': '75', 'left': '5'}),
+        html.A(html.Button('Baixar Dados',className = 'menuButton'),id = 'baixar_dados',download = 'focos_de_calor.csv'),html.Br(),
+        html.A(html.Button('Baixar Mapa',className = 'menuButton'),id = 'baixar_mapa',download = 'focos_de_calor.html'),html.Br(),
+        html.A(html.Button('Dashboard',className = 'menuButton',id = 'dashButton',n_clicks = 0)),html.Br(),
+        html.A(html.Button('Sobre',id = 'sobre',className = 'menuButton',n_clicks = 0))],
+        style = {'position': 'fixed', 'top': '0', 'left': '0','margin-top':'75px','margin-left':'10px'}),
     dcc.Graph(
         id='mapa',
         figure = inicial_figure(),
         style = {'position': 'fixed', 'top': '0', 'left': '0','height': '100vh', 'width': '100vw', 'z-index': '-1'}
     ),
-    html.Div(id = 'fade'),
+    html.Div(id = 'fade',className = 'fade'),
     html.Div([
-        html.Button('X',id = 'closeInfo',n_clicks = 0),
+        html.Button('X',id = 'closeInfo',n_clicks = 0,className = 'close'),
         html.Div([
             html.H2('Sobre o projeto'),
             html.P('Dados de satélite das últimas 24h.'),
             html.P(['Fonte dos dados: ',html.A('BDQueimadas',href = 'https://queimadas.dgi.inpe.br/queimadas/portal')]),
             html.P(['Criado por: ',html.A('Aruã Viggiano Souza',href = 'https://www.linkedin.com/in/aru%C3%A3-viggiano-souza/')]),
             html.P(['Código fonte: ',html.A('Github',href = 'https://github.com/aruasouza/focos-de-calor-dash')])
-        ],id = 'blocoTexto')],id = 'info')
+        ],className = 'blocoTexto')],id = 'info'),
+    html.Div(id = 'fade2',className = 'fade'),
+    html.Div([
+        html.Button('X',id = 'closeDash',n_clicks = 0,className = 'close'),
+        html.Div([
+            html.H2('Dashboard'),
+            dcc.Graph(id = 'byCountry'),
+            dcc.Graph(id = 'bySatelite'),
+        ],className = 'blocoTexto')],id = 'dashboard'),
+    dcc.Checklist(id = 'modo_escuro',options = ['Modo Escuro'],style = {'position': 'fixed', 'top':'93%','left':'1%','color':'rgb(100,100,100)',
+                                           'background-color':'white','padding':'5px','border-radius':'10px','font-family':'helvetica'})
 ])
 
 if __name__ == '__main__':
-    app.run_server(debug=False,port = 80,host = '0.0.0.0')
+    app.run_server(debug=True,port = 80)#,host = '0.0.0.0')
